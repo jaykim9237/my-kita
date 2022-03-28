4ff757f388f33ccf708d02cb25a8d8802702af42
hyunjung_jay <jaykim9237@gmail.com>
2022-03-28 19:51:48 +0900

    03.28 chat

---
+++ chatapp/src/main/java/com/cos/chatapp/Chat.java
@@ -0,0 +1,28 @@
+package com.cos.chatapp;
+
+
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection="chat")
+public class Chat {
+
+	@Id
+	private String id;
+	private String msg;
+	private String sender; //보내는 사람
+	private String receiver; //받는 사람
+	
+	private LocalDateTime createAt;
+
+	public void setCreatedAt(LocalDateTime now) {
+		// TODO Auto-generated method stub
+		
+	}
+}
